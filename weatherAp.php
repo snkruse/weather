4ee7893f361acc485f5531e1e7b792410f5fea1e
snkruse <sethkruse@gmail.com>
2016-06-19 00:58:30 -0700

    CHECKPOINT: able to select a city, pass city to script, build url & receive response and parse file to display on webpage

--- weatherAp.php
+++ weatherAp.php
@@ -1,17 +1,11 @@
 <!DOCTYPE HTML>
 <html lang = 'en'>
 <head>
- 	
  	<script src="weatherReply.js" type="text/javascript"></script> 
-	
 	<meta character = 'UTF=8'>
 	<title>Weather Application</title>
 </head>
 <body>
-
-
-
-
 	<form NAME="testform">
 		<input TYPE="button" NAME="button" Value="Click" 
 		    onClick="getCity(this.form)"><BR>
@@ -20,10 +14,7 @@
 		<input TYPE="checkbox" NAME="rad" Value="San Francisco"	>San Francisco	<BR>
 	</form>
 
-
-
-
-	<img src="../weather/images/sun.png"/>
+<!-- 	<img src="../weather/images/sun.png"/> -->
 
 	<div id = 'weather'></div>
 	<p><span id = "temp"></span></p>
--- weatherReply.js
+++ weatherReply.js
@@ -1,37 +1,37 @@
 
 <!--
+	var url = "http://api.wunderground.com/api/c93f93adfe76ed29/conditions/q/CA/";
+
 	function getCity (form){
-	helloWorld();
-    for (count = 0; count < 3; count++) {
-        if (form.rad[count].checked)
-            {
-            	var text = form.rad[count].value;
-            	alert(text);
-            }
-    }
-    
-	}
-	function helloWorld(){
-		alert("hello world called");
+	    for (count = 0; count < 3; count++) {
+	        if (form.rad[count].checked)
+	            {
+	            	var text = form.rad[count].value;
+	            	buildRequest(text);
+	            }
+	    }
 	}
 
-	// var weather = new XMLHttpRequest();
-	// weather.open("GET", "http://api.wunderground.com/api/c93f93adfe76ed29/conditions/q/CA/Poway.json", false); // false for synchronous request
-	// weather.send(null); // we are not going to send anything to the weatheronline api
-	// var text = "";
+	function buildRequest(city){
+		url += "/" + city + ".json";
 
-	// var r    = JSON.parse(weather.response);
-	// var dis  = "Current Location: " + r.current_observation.display_location.full + "<br />";
-	// var temp = r.current_observation.temperature_string + "<br />";
-	// var wind = r.current_observation.wind_string + "<br />";
-	// //document.getElementById("weather").innerHTML = dis;
-	// //document.getElementById("weather").innerHTML = temp;
-	// //document.getElementById("weather").innerHTML = wind;
-	
-	// var weatherList = [dis,temp,wind];
-	// text = getWeatherData(weatherList, text);
-	// document.getElementById("weather").innerHTML = text;
+		var weather = new XMLHttpRequest();
+		weather.open("GET", "http://api.wunderground.com/api/c93f93adfe76ed29/conditions/q/CA/Poway.json", false); // false for synchronous request
+		weather.send(null); // we are not going to send anything to the weatheronline api
+		var text = "";
 
+		var r    = JSON.parse(weather.response);
+		var dis  = "Current Location: " + r.current_observation.display_location.full + "<br />";
+		var temp = r.current_observation.temperature_string + "<br />";
+		var wind = r.current_observation.wind_string + "<br />";
+		//document.getElementById("weather").innerHTML = dis;
+		//document.getElementById("weather").innerHTML = temp;
+		//document.getElementById("weather").innerHTML = wind;
+		
+		var weatherList = [dis,temp,wind];
+		text = getWeatherData(weatherList, text);
+		document.getElementById("weather").innerHTML = text;
+	}
 
 	function getWeatherData(variable1, variable2){
 		 for(var i = 0; i < variable1.length;  i++)
