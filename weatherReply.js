997d5ffec87e52479420e0e81f9865649e76f409
snkruse <sethkruse@gmail.com>
2016-06-18 18:43:59 -0700

    connected to my weatherundergound api and got json response and now parsing the file

--- weatherReply.js
+++ weatherReply.js
@@ -1,30 +1,13 @@
-<!DOCTYPE HTML>
-<html lang = 'en'>
-<head>
-	<meta character = 'UTF=8'>
-	<title>Weather Application</title>
-</head>
-<body>
-	Hello World
-	<div id = 'weather'></div>
-	<p><span id = "temp"></span></p>
-	<p><span id = "wind"></span></p>
-
-	<script src = "weatherReply.js"></script>
 
 <!--
-<script>
 	var weather = new XMLHttpRequest();
 	weather.open("GET", "http://api.wunderground.com/api/c93f93adfe76ed29/conditions/q/CA/San_Francisco.json", false); // false for synchronous request
 	weather.send(null); // we are not going to send anything to the weatheronline api
 	var r    = JSON.parse(weather.response);
 	var dis  = "Current Location: " + r.current_observation.display_location.full + "<br />";
 	var temp = r.current_observation.temperature_string + "<br />";
-	var wind = r.current_observation.wind_string + <"br />";
+	var wind = r.current_observation.wind_string + "<br />";
 	document.getElementById("weather").innerHTML = dis;
-	window.alert(6);
-</script>
+	document.getElementById("weather").innerHTML = temp;
+	document.getElementById("weather").innerHTML = wind;
 -->
-
-</body>
-</html>
