3a4ecbc12a630e6a22ed38b9dd55331b3c78a75a
snkruse <sethkruse@gmail.com>
2016-06-19 00:42:52 -0700

    created a form that grabs 'this' and gets checked item

---
+++ getcity.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<head>
+	<title></title>
+</head>
+<body>
+
+<b>Add a New Student</b>
+ 
+
+ <select>
+  <option value="Poway">Poway</option>
+  <option value="Rancho_Penasquitos">Rancho Penaquitos</option>
+  <option value="Del_mar">Del Mar</option>
+</select> 
+ 
+<p>
+<input type="submit" name="submit" value="Send" />
+</p>
+
+</body>
+</html>
--- weatherAp.php
+++ weatherAp.php
@@ -1,16 +1,34 @@
 <!DOCTYPE HTML>
 <html lang = 'en'>
 <head>
+ 	
+ 	<script src="weatherReply.js" type="text/javascript"></script> 
+	
 	<meta character = 'UTF=8'>
 	<title>Weather Application</title>
 </head>
 <body>
-	Hello World
+
+
+
+
+	<form NAME="testform">
+		<input TYPE="button" NAME="button" Value="Click" 
+		    onClick="getCity(this.form)"><BR>
+		<input TYPE="checkbox" NAME="rad" Value="San Diego"		>San Diego		<BR>
+		<input TYPE="checkbox" NAME="rad" Value="Poway"			>Poway			<BR>
+		<input TYPE="checkbox" NAME="rad" Value="San Francisco"	>San Francisco	<BR>
+	</form>
+
+
+
+
+	<img src="../weather/images/sun.png"/>
+
 	<div id = 'weather'></div>
 	<p><span id = "temp"></span></p>
 	<p><span id = "wind"></span></p>
 
-	<script src = "weatherReply.js"> 	</script>
 
 
 
